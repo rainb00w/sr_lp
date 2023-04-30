9945543f450e8ed7cef192529e68b96eda47edb5
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-05-01 02:25:34 +0300

    _v1n

--- src/layout/Form.tsx
+++ src/layout/Form.tsx
@@ -17,6 +17,21 @@ export default function Form(): JSX.Element {
   //   console.log('submitted', data);
   // };
 
+  const onSubmit = (data: FormValues) => {
+    // event.preventDefault();
+
+    // const myForm = event.target;
+    // const formData = new FormData(myForm);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(data).toString(),
+    })
+      .then(() => console.log('Form successfully submitted'))
+      .catch((error) => alert(error));
+  };
+
   return (
     <>
       <div id="contant" className={s.form}>
